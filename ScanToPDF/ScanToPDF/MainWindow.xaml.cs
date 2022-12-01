d546b8615cde4c4b8e7344549e1c1ebfecd9eea2
Emanuele <paciemanuele.2@gmail.com>
2022-12-01 21:35:13 +0100

    lettura dei device

--- ScanToPDF/ScanToPDF/MainWindow.xaml.cs
+++ ScanToPDF/ScanToPDF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WIA;
 
 namespace ScanToPDF
 {
@@ -20,9 +21,32 @@ namespace ScanToPDF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DeviceManager deviceManager = new DeviceManager();
+        private List<Scanner> scannerList = new List<Scanner>();
         public MainWindow()
         {
             InitializeComponent();
+            Console.WriteLine();
+
+            for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+            {
+                if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                    continue;
+
+                scannerList.Add(new Scanner((string)deviceManager.DeviceInfos[i].Properties["Name"].get_Value(),
+                    (string)deviceManager.DeviceInfos[i].Properties["Description"].get_Value(),
+                    (string)deviceManager.DeviceInfos[i].Properties["Port"].get_Value()));
+            }
+            cmbScanners.ItemsSource = scannerList;
+        }
+
+        private void cmbScanners_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = cmbScanners.SelectedIndex;
+            String info = "Name: " + scannerList.ElementAt(index).getName() + "\n" +
+                "Description: " + scannerList.ElementAt(index).getDescription() + "\n" +
+                "Port: " + scannerList.ElementAt(index).getPort();
+            Console.WriteLine(info);
         }
     }
 }
---
+++ ScanToPDF/ScanToPDF/Scanner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanToPDF
+{
+    class Scanner
+    {
+        private string name;
+        private string description;
+        private string port;
+
+        public Scanner(string name, string description, string port)
+        {
+            this.name = name;
+            this.description = description;
+            this.port = port;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+
+        public string getPort()
+        {
+            return port;
+        }
+
+        public override string ToString()
+        {
+            return getName();
+        }
+    }
+}
